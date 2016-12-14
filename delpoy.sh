7e375dadf656ff539a2651e008709508392d5416
Anton Marin <anton.marin@skolkovotech.ru>
2016-12-14 18:42:03 +0300

    current state during talk with Nadya

--- delpoy.sh
+++
@@ -1,9 +0,0 @@
-cluster_hostname=team24@nla24project1-ssh.azurehdinsight.net
-
-if [ ! $# == 1 ]
-then
-  echo "Please specify class name"
-  exit
-fi
-
-sbt package && scp target/scala-2.11.7/nla-project_2.11.7-1.0.jar $cluster_hostname:/home/team24 && ssh $cluster_hostname spark-submit --class "$1" --master yarn nla-project_2.11.7-1.0.jar
